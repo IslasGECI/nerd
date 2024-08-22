2f56a90b42ab97348c5f3bfbd75ee6bb5a829f3e
Mario Villasante <mario.villasante@islas.org.mx>
2024-08-22 14:25:25 -0700

    🚚✨ Change from setup to pyproject

--- nerd/__init__.py
+++ nerd/__init__.py
@@ -1,1 +1,4 @@
-from nerd.solver import solver  # noqa
+"""The NERD algorithm performs calculations with increased accuracy, displaying results almost in real-time."""
+
+__version__ = "0.4.0"
+from .solver import solver  # noqa
